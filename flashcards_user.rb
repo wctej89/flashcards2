fe961dcb26a772f39491e6ca9da9e8db727feb41
alukima <amylukima@gmail.com>
2013-05-24 14:43:33 -0700

    Finish UserInterface

--- flashcards_user.rb
+++ flashcards_user.rb
@@ -1,5 +1,36 @@
+require_relative 'card-deck-classes'
+require_relative 'flashcards_parser'
+
+
+
 class UserInterface
 
+  NUMBER_OF_GUESSES = 3
+
+
+
+  def self.run(deck)
+
+    puts "Welcome to Ruby Flash Cards. To play, just enter the correct term for each definition.  Ready?  Go!"
+    
+    until deck.empty?
+      card = deck.next
+      puts card.definition
+      NUMBER_OF_GUESSES.times do |x|
+        print 'Guess: '
+        guess = gets.chomp
+        if card.match?(guess)
+          puts 'Correct!'
+          break
+        elsif x == NUMBER_OF_GUESSES - 1
+          puts "Incorrect. The answer is #{card.term}"
+        else
+          puts "Incorrect"
+        end
+      end
+    end
+  
+  end
 end
 
 
@@ -13,7 +44,11 @@ end
 
 deck = Deck.new
 
+
 parser = Parser.new(filename: 'flashcard_samples.txt', contents_class: Card, 
                     container: deck)
 
 parser.populate_container!
+deck.shuffle!
+UserInterface.run(deck)
+
