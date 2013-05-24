851eaadcea79fe8ed3bc8ff39a9b9f1de38195e9
alukima <amylukima@gmail.com>
2013-05-24 12:17:15 -0700

    Add populate_container! method to Parser

--- flashcards_parser.rb
+++ flashcards_parser.rb
@@ -7,8 +7,15 @@ class Parser
   #            container_instance: deck_instance)
   def initialize(args)
     @filename = args[:filename]
-    @contents_class = args[:contents_class]
-    @container_instance = args[:container_instance]
+    @contents_class = args[:contents_class] || nil
+    @container = args[:container] || nil
+  end
+
+  def populate_container!
+    File.readlines(@filename, "\n\n").each do |linepair|
+      definition, term = linepair.split("\n")
+      self.container.insert!(Card.new(definition: definition, term: term))
+    end
   end
 
 end
